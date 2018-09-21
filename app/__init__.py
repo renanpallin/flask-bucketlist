ac59d1dc056146d5e74df96b07434382e80edcb4
Renan Pallin <renanpallin@gmail.com>
2018-09-21 14:48:37 -0300

    Finish

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
+from flask import request
+
 # local import
 from instance.config import app_config
 
@@ -8,10 +10,24 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
+    from app.models import Bucketlist
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    @app.route('/bucketlists/')
+    def bucketlists():
+        return Bucketlist.get_all()
+
+    @app.route('/bucketlists/', methods=['POST'])
+    def bucketlists_post():
+        data = request.get_json()
+        b = Bucketlist(data.get('name'))
+
+        print(b)
+        return {'aa': 123}
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,6 +1,37 @@
 from app import db
+from flask_bcrypt import Bcrypt
 
-class Bucketlist(db.Model):
+# class User(db.Model):
+# 	"""This class defines the users table"""
+
+# 	__table__ = 'users'
+
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	email = db.Column(db.String(256), nullable=False, unique=True)
+# 	password = db.Column(db.String(256), nullable=False)
+# 	bucketlists = db.relationship('Bucketlist', order_by='Bucketlist.id', cascade="all, delete-orphan")
+
+# 	def __init__(self, email, password):
+# 		self.email = email
+# 		self.password = password
+
+# 	def password_is_valid(self, password):
+# 		return Bcrypt().check_password_hash(self.password, password)
+
+# 	def save(self):
+# 		db.session.add(self)
+# 		db.session.commit()
+
+
+class SerializableModel(db.Model):
+
+	def __call__(self):
+		return {'a': 666}
+	# def as_dict(self):
+ #       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Bucketlist(SerializableModel):
 	"""This class represents the bucketlist table."""
 
 	__tablename__ = 'bucketlists'
@@ -11,6 +42,7 @@ class Bucketlist(db.Model):
 	date_modified = db.Column(
 		db.DateTime, default=db.func.current_timestamp(),
 		onupdate=db.func.current_timestamp())
+	# created_by = db.Column(db.Integer, db.ForeignKey(User.id))
 
 	def __init__(self, name):
 		"""Initialize with name."""
@@ -21,7 +53,7 @@ class Bucketlist(db.Model):
 		db.session.commit()
 
 	@staticmethod
-	def get_all(self):
+	def get_all():
 		return Bucketlist.query.all()
 
 	def delete(self):
